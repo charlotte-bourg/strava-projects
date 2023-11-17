88f9fb4d01a15205d3d45a445b55816e962ed6c0
charlotte-bourg <charlotte.bourg@gmail.com>
2023-11-17 13:15:22 -0800

    testing webhook creation & email generation

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 """Server for running helper app."""
 import os 
 import requests
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify 
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_KEY']
@@ -9,12 +10,29 @@ app.secret_key = os.environ['FLASK_KEY']
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 REDIRECT_URI = os.environ['REDIRECT_URI']
+STRAVA_VERIFY_TOKEN = os.environ['STRAVA_VERIFY_TOKEN']
 
 AUTHORIZE_URL = 'https://www.strava.com/oauth/authorize'
 TOKEN_URL = 'https://www.strava.com/api/v3/oauth/token'
 DEAUTHORIZE_URL = 'https://www.strava.com/oauth/deauthorize'
 
 SCOPES = 'read'
+
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'stravagearupdater@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ['EMAIL_PASS']
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route('/test-email')
+def test_email():
+    msg = Message('Hello from strava gear updater', sender = 'stravagearupdater@gmail.com', recipients = ['charlotte.bourg@gmail.com'])
+    msg.body = "testing from flask-mail"
+    mail.send(msg)
+    return "sent"
 
 @app.route('/')
 def home():
@@ -49,6 +67,40 @@ def callback():
     # session['refresh_token'] = token_data['refresh_token']
 
     return f'Authentication successful! Welcome {token_data["athlete"]["firstname"]}!'
+
+@app.route('/update-gear/create-webhook')
+def create_webhook():
+    webhook_data = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'callback_url': 'https://c2d6-23-118-109-73.ngrok.io/webhook',
+        'verify_token': STRAVA_VERIFY_TOKEN
+    }
+
+    response = requests.post('https://www.strava.com/api/v3/push_subscriptions', data=webhook_data)
+    subscription_data = response.json()
+
+    print(subscription_data)
+
+    return f'Webhook subscription created?'
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def strava_webhook():
+    print("*****HEYHELLOHI*****")
+    if request.method == 'GET':
+        hub_challenge = request.args.get('hub.challenge','')
+        hub_verify_token = request.args.get('hub.verify_token', '')
+        print("hello")
+        if hub_verify_token == STRAVA_VERIFY_TOKEN:
+            return jsonify({'hub.challenge': hub_challenge})
+        else:
+            return 'Invalid verify token', 403
+    else:
+        data = request.json
+        if data.get('hub.verify_token') == STRAVA_VERIFY_TOKEN:
+            return jsonify({'hub.challege': data.get('hub.challenge')})
+        else:
+            return 'Invalid verify token', 403
 
 if __name__ == '__main__':
     # connect_to_db(app)
